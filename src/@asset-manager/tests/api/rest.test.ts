eceb3b595de57333e5cd04d90a2460aa90a8bc35
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-03-12 11:38:38 +0330

    final commit

--- src/@asset-manager/tests/api/rest.test.ts
+++
@@ -1,9 +0,0 @@
-import { Rest } from '../../services';
-
-describe('running apis', function () {
-  test('testing rest api', async function () {
-    const res = await Rest.req({ url: 'https://jsonplaceholder.typicode.com/posts/1' });
-
-    expect(res.userId).toBe(1);
-  });
-});
